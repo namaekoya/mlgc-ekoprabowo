6496a37ec97219076bc10d994fa1050d0671f33f
Eko Prabowo <57608542+namaekoya@users.noreply.github.com>
2024-05-23 15:21:07 +0700

    Add files via upload

---
+++ sertaMulia-lab/src/server/routes.js
@@ -0,0 +1,17 @@
+const postPredictHandler = require('./handler');
+
+const routes = [
+  {
+    path: '/predict',
+    method: 'POST',
+    handler: postPredictHandler,
+    options: {
+      payload: {
+        allow: 'multipart/form-data',
+        multipart: true
+      }
+    }
+  }
+]
+
+module.exports = routes;
